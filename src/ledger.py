34f82f119e298cc4a9cb20e5134803e2e78b4da9
seungwon <stevenswjoeng@gmail.com>
2022-09-01 16:40:19 +0900

    redesign structures

--- src/ledger.py
+++ src/ledger.py
@@ -1,5 +1,6 @@
 # @author: seungw0n
 from datetime import datetime
+from neis import neisLog
 from excel import openExcel, readLedger
 from design import Restaurant
 
@@ -26,15 +27,22 @@ def createObj(excelResult: dict) -> dict:
                     result[k] = [r]
     
     return result
-            
 
 
-
-def ledgerLog(filename: str):
+def ledgerLog(filename: str) -> dict:
     wb, sheetnames = openExcel(filename=filename)
+
+    for sheetname in sheetnames:
+        sheet = wb[sheetname]
+
+        
+
+
+
+
     excelResult = readLedger(wb, sheetnames)
     
-    result = createObj(excelResult)
+    result = createObj(excelResult)  # {date: [Restaurant, Restaurant, ....], ....}
 
     for k, v in result.items():
         print(k)
@@ -42,5 +50,37 @@ def ledgerLog(filename: str):
             print("\t" + str(e))
 
 
+    return result
 
-ledgerLog("특근매식비장부.xlsx")
+    
+
+# def createFinalLog(neisFilename: str, ledgerFilename: str) -> tuple:
+#     neisValidLog, neisInvalidLog = neisLog(filename=neisFilename)
+#     restaurantLog = ledgerLog(filename=ledgerFilename)
+
+#     valid = list()
+#     invalid = list()
+
+#     # check if employee is able to eat
+#     for date, restuarantObjs in restaurantLog:
+
+#         if len(restuarantObjs) == 0:
+#             continue
+
+#         overWorks = neisValidLog[date]
+        
+#         if len(overWorks) == 0:
+#             invalid.append([date, restuarantObjs])
+#             continue
+        
+#         for r in restuarantObjs:
+#             names = r.employeeNames
+
+#             for name in names:
+#                 for overWork in overWorks:
+                    
+
+    
+c, w = neisLog("./files/초과근무확인8월분.xlsx", 16, 50)
+print(c)
+ledgerLog("./files/8월 특근매식비장부.xlsx")
